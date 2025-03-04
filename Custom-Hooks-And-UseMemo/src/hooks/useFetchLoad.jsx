a90a15ba1673a2f931bdd5dd75edfdf0c2784ad9
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2025-03-04 17:16:16 +0530

    React

--- Custom-Hooks-And-UseMemo/src/hooks/useFetchLoad.jsx
+++ Custom-Hooks-And-UseMemo/src/hooks/useFetchLoad.jsx
@@ -31,3 +31,41 @@ const useFetchLoad = (url) => {
 }
 
 export default useFetchLoad
+
+
+// import { useEffect, useState } from "react";
+
+// const useFetch = (url) => {
+//     const [loading, setLoading] = useState(true);
+//     const [data, setData] = useState(null);
+//     const [error, setError] = useState(null);
+
+//     useEffect(() => {
+//         let isMounted = true; // Prevent state update after unmount
+
+//         const fetchData = async () => {
+//             setLoading(true);
+//             try {
+//                 let res = await fetch(url);
+//                 if (!res.ok) throw new Error(`HTTP Error: ${res.status}`);
+//                 let bag = await res.json();
+//                 if (isMounted) setData(bag);
+//             } catch (err) {
+//                 if (isMounted) setError(err);
+//                 console.error(err);
+//             } finally {
+//                 if (isMounted) setLoading(false);
+//             }
+//         };
+
+//         fetchData();
+
+//         return () => {
+//             isMounted = false;
+//         };
+//     }, [url]);
+
+//     return { loading, data, error };
+// };
+
+// export default useFetch;
